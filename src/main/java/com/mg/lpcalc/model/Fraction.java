35ed32fbcb869a9341f2db855dbf246b39b6f5af
MikeGambino <dan.k-2011@yandex.ru>
2025-03-03 02:24:33 +0400

    add Fraction methods

--- src/main/java/com/mg/lpcalc/model/Fraction.java
+++ src/main/java/com/mg/lpcalc/model/Fraction.java
@@ -2,12 +2,13 @@ package com.mg.lpcalc.model;
 
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data
 public class Fraction {
     private int numerator;
     private int denominator;
 
-    // Конструкторы
     public Fraction() {
         this.numerator = 0;
         this.denominator = 1;
@@ -15,13 +16,94 @@ public class Fraction {
 
     public Fraction(int numerator, int denominator) {
         if (denominator == 0) {
-            throw new IllegalArgumentException("Знаменатель не может быть нулем");
+            throw new IllegalArgumentException("Denominator cannot be zero");
         }
         this.numerator = numerator;
         this.denominator = denominator;
+        normalize();
+    }
+
+    private void normalize() {
+        if (numerator == 0) {
+            denominator = 1;
+            return;
+        }
+
+        if (denominator < 0) {
+            numerator = -numerator;
+            denominator = -denominator;
+        }
+
+        int gcd = gcd(Math.abs(numerator), denominator);
+        numerator /= gcd;
+        denominator /= gcd;
+    }
+
+    // Поиск НОД (Алгоритм Евклида)
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public Fraction add(Fraction other) {
+        int newNumerator = this.numerator * other.denominator + other.numerator * this.denominator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction subtract(Fraction other) {
+        int newNumerator = this.numerator * other.denominator - other.numerator * this.denominator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction multiply(Fraction other) {
+        int newNumerator = this.numerator * other.numerator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction divide(Fraction other) {
+        if (other.numerator == 0) {
+            throw new IllegalArgumentException("Division by zero");
+        }
+        int newNumerator = this.numerator * other.denominator;
+        int newDenominator = this.denominator * other.numerator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    // Отрицательная версия дроби
+    public Fraction negate() {
+        return new Fraction(-this.numerator, this.denominator);
     }
 
     public Double doubleValue() {
         return (double) (numerator / denominator);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fraction fraction = (Fraction) o;
+        return numerator == fraction.numerator && denominator == fraction.denominator;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numerator, denominator);
+    }
+
+    @Override
+    public String toString() {
+        if (denominator == 1) {
+            return Integer.toString(numerator);
+        } else {
+            return numerator + "/" + denominator;
+        }
     }
 }
